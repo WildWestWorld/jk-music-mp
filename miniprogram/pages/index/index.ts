ea54b4d3b2bebc945f97d8e7db4662a7432f55bf
HorusEY3s <380686356@qq.com>
2022-01-23 15:22:58 +0800

    first commit

---
+++ miniprogram/pages/index/index.ts
@@ -0,0 +1,27 @@
+// index.ts
+
+import { sayHello } from "../../api/hello"
+
+// 获取应用实例
+const app = getApp<IAppOption>()
+
+Page({
+  data: {
+
+    message:'hello world!'
+  },
+  // 事件处理函数
+  //button点击 会调用hi函数
+  hi(){
+    //因为返回回来的是promise对象所以可以.then
+    sayHello().then(resData =>{
+   
+      //微信中的赋值
+      this.setData!({
+      message : resData
+     })
+     
+      return console.log(resData)
+    })
+  }
+})
